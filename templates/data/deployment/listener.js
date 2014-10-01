faecbef7f9cd2345d332ce78edaedd9277516683
Chris Olstrom <chris@olstrom.com>
2014-09-30 17:22:05 -0700

    Renamed webhooks in dependencies.

---
+++ templates/data/deployment/listener.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+
+var cluster = require('cluster'),
+    port = 30080;
+
+function record_pidfile_at(pidfile_path) {
+  try {
+    var pid = require('npid').create(pidfile_path);
+    pid.removeOnExit();
+  } catch(error) {
+    console.log(error);
+    process.exit(1);
+  }
+}
+
+if (cluster.isMaster) {
+  record_pidfile_at('/data/run/autodeploy/listener.pid');
+  cluster.on('exit', function(worker, code, signal) {
+    cluster.fork();
+  });
+  cluster.fork();
+} else {
+  require('http').createServer(function(request, response) {
+    response.statusCode = 200;
+    response.write('<pre>');
+    ansible = require('child_process').spawn('/usr/local/bin/ansible-playbook', ['/data/deployment/deploy.yml']);
+    ansible.stdout.pipe(response);
+    ansible.stdout.on('end', function() {
+      response.end('</pre>');
+      
+    })
+  }).listen(port);
+}
---
+++ templates/data/deployment/listener/deploy/index.php
@@ -0,0 +1,1 @@
+<pre><?php echo shell_exec('/usr/local/bin/ansible-playbook /data/deployment/deploy.yml'); ?></pre>
