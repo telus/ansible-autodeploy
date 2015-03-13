745cc1090ed281c8bbc1245002f22a6a0490518d
Ben Visser <theodore.r.visser@gmail.com>
2015-03-13 11:50:59 -0700

    fixed security issues with dynamic deployment :x

--- templates/data/deployment/listener.js
+++ templates/data/deployment/listener.js
@@ -3,7 +3,9 @@
 // {{ ansible_managed }}
 
 var cluster = require('cluster'),
-    port = 30080;
+  fs = require('fs'),
+  qs = require('qs'),
+  port = 30080;
 
 function record_pidfile_at(pidfile_path) {
   try {
@@ -16,20 +18,95 @@ function record_pidfile_at(pidfile_path) {
 }
 
 if (cluster.isMaster) {
-  record_pidfile_at('/data/run/autodeploy/listener.pid');
+  record_pidfile_at('{{ autodeploy_pidfile_path }}');
   cluster.on('exit', function(worker, code, signal) {
     cluster.fork();
   });
   cluster.fork();
 } else {
   require('http').createServer(function(request, response) {
-    response.statusCode = 200;
-    response.write('<pre>');
-    ansible = require('child_process').spawn('/usr/local/bin/ansible-playbook', ['/data/deployment/deploy.yml']);
-    ansible.stdout.pipe(response);
-    ansible.stdout.on('end', function() {
-      response.end('</pre>');
 
-    })
+// {% if autodeploy_dynamic_deploy_enabled %}
+
+  switch (request.method) {
+    case 'POST':
+      var body;
+      request.on('data', function (data) {
+        body += data
+      });
+      request.on('end', function () {
+        response.statusCode = 200;
+        response.write('<pre>');
+
+        var payload = qs.parse(body)["undefinedpayload"];
+        var post = JSON.parse(payload);
+
+        var repo = post["repository"]["name"];
+        switch(repo){
+
+      // {% for repo in autodeploy_dynamic_deploy_repo_names %}   
+
+          case '{{ repo }}':
+            var ansible = require('child_process').spawn('/usr/local/bin/ansible-playbook', ["/data/deployment/{{ repo }}.yml"]);
+            ansible.stdout.pipe(response);
+            ansible.stdout.on('end', function() {
+              response.end('</pre>');
+            });
+          break;
+
+      // {% endfor%}
+
+        }
+      });
+    break;
+    case 'GET':
+      response.statusCode = 200;
+      response.write('<pre>');
+
+      switch(request.url) {
+        case '/':
+          var arr = [];
+          fs.readdir('/data/deployment', function(err, files) {
+            files.filter(function(item) {
+              if(/\.yml/.test(item)) {
+                arr.push(item);
+              }
+            })
+            ansible = require('child_process').spawn('/usr/local/bin/ansible-playbook', arr);
+            ansible.stdout.pipe(response);
+            ansible.stdout.on('end', function() {
+              response.end('</pre>');
+            });
+          })
+          break;
+
+      // {% for repo in autodeploy_dynamic_deploy_repo_names %}
+
+        case '/{{ repo }}':
+          ansible = require('child_process').spawn('/usr/local/bin/ansible-playbook', ["/data/deployment/{{ repo }}.yml"]);
+          ansible.stdout.pipe(response);
+          ansible.stdout.on('end', function() {
+            response.end('</pre>');
+          });
+          break;
+
+      // {% endfor%}
+
+      }
+    break;
+  }
+  
+// {% else %}
+
+  response.statusCode = 200;
+  response.write('<pre>');
+  ansible = require('child_process').spawn('/usr/local/bin/ansible-playbook', ['/data/deployment/deploy.yml']);
+  ansible.stdout.pipe(response);
+  ansible.stdout.on('end', function() {
+    response.end('</pre>');
+  })
+
+// {% endif %}
+
   }).listen(port);
 }
--- templates/data/deployment/listener/index.php
+++ templates/data/deployment/listener/index.php
@@ -1,1 +1,47 @@
-<pre><?php echo shell_exec('/usr/local/bin/ansible-playbook /data/deployment/deploy.yml'); ?></pre>
+<pre><?php
+// {% if autodeploy_dynamic_deploy_enabled %}
+
+if($_SERVER['REQUEST_METHOD'] === 'POST') {
+  $data = file_get_contents('php://input');
+  $post = json_decode($data, TRUE);
+  $repo = $post['repository']['slug'];
+
+  switch($repo) {
+  // {% for repo in autodeploy_dynamic_deploy_repo_names %}   
+
+    case '{{ repo }}':
+      echo shell_exec('/usr/local/bin/ansible-playbook /data/deployment/{{ repo }}.yml');
+      break;
+
+  // {% endfor%}
+
+  }
+} else {
+  $request_uri = $_SERVER["REQUEST_URI"];
+  switch($request_uri) {
+    case '/':
+      $dir_files = scandir('/data/deployment');
+      foreach($dir_files as $file){
+        if (strpos($file,'.yml') !== false) {
+          echo shell_exec('/usr/local/bin/ansible-playbook /data/deployment/'.$file);
+        }
+      }
+      break;
+  // {% for repo in autodeploy_dynamic_deploy_repo_names %}
+
+    case '/{{ repo }}':
+      echo shell_exec('/usr/local/bin/ansible-playbook /data/deployment/{{ repo }}.yml');
+      break;
+
+  // {% endfor%}
+
+  } 
+}
+
+// {% else %}
+
+echo shell_exec('/usr/local/bin/ansible-playbook /data/deployment/deploy.yml');
+
+// {% endif %}
+
+?></pre>
