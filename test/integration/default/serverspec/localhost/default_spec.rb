08d096640fbd3db9e0536e61effa901f1b7b9de2
Ben Visser <theodore.r.visser@gmail.com>
2016-01-19 15:27:28 -0500

    adding required vars

--- test/integration/default/serverspec/localhost/default_spec.rb
+++ test/integration/default/serverspec/localhost/default_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe 'ansible-autodeploy::default' do
+  describe file('/data/deployment/deploy.pub') do
+    it { should exist }
+  end
 
+  describe file('/data/deployment/deploy.key') do
+    it { should exist }
+  end
 end
