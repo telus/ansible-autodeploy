6f68ed2ecf24713667aaf8a2ad10c7fc4dcf866d
Steven Harradine <stevenharradine@gmail.com>
2017-04-05 12:45:20 -0400

    use autodeploy_listener_port in all the places

--- templates/data/deployment/update-app-code.sh
+++ templates/data/deployment/update-app-code.sh
@@ -3,11 +3,11 @@
 deploy_code ()
 {
   echo -n 'trying to deploy code '
-  is_autodeploy_up=`netstat -lnt | grep ":81 " | wc -l`
+  is_autodeploy_up=`netstat -lnt | grep ":{{ autodeploy_listener_port }} " | wc -l`
   
   if [ $is_autodeploy_up -ge 1 ]; then
     echo 'hit'
-    curl --request POST localhost:81
+    curl --request POST localhost:{{ autodeploy_listener_port }}
   else
     echo 'miss'
     sleep 5s
